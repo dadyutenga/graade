c4262f5989d9b32885f95c49fced63dc426d4645
dady  utenga <dadyutenga@yahoo.com>
2025-03-08 17:54:13 +0300

    Add second database connection configuration

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -51,6 +51,38 @@ class Database extends Config
         ],
     ];
 
+    /**
+     * Second database connection for application data.
+     *
+     * @var array<string, mixed>
+     */
+    public array $second_db = [
+        'DSN'          => '',
+        'hostname'     => 'localhost',
+        'username'     => 'root',
+        'password'     => '123456789',
+        'database'     => 'elliot', // Different database name
+        'DBDriver'     => 'MySQLi',
+        'DBPrefix'     => '',
+        'pConnect'     => false,
+        'DBDebug'      => true,
+        'charset'      => 'utf8mb4',
+        'DBCollat'     => 'utf8mb4_general_ci',
+        'swapPre'      => '',
+        'encrypt'      => false,
+        'compress'     => false,
+        'strictOn'     => false,
+        'failover'     => [],
+        'port'         => 3306,
+        'numberNative' => false,
+        'foundRows'    => false,
+        'dateFormat'   => [
+            'date'     => 'Y-m-d',
+            'datetime' => 'Y-m-d H:i:s',
+            'time'     => 'H:i:s',
+        ],
+    ];
+
     //    /**
     //     * Sample database connection for SQLite3.
     //     *
